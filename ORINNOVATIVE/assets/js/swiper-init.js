6e11bdd795939a907c9a3bb4fb6ea957f7427a5f
coderadi2007-spec <coderadi2007@gmail.com>
2026-06-04 03:22:42 +0530

    Delete ORINNOVATIVE directory

--- ORINNOVATIVE/assets/js/swiper-init.js
+++
@@ -1,7 +0,0 @@
-new Swiper('.testimonial-swiper', {
-  loop: true,
-  autoplay: { delay: 3000 },
-  pagination: { el: '.swiper-pagination', clickable: true },
-  slidesPerView: 1,
-  breakpoints: { 768: { slidesPerView: 2 }, 1024: { slidesPerView: 3 } }
-});
